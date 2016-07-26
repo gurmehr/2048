94430f68cbfca563934f6742db1ea7db8908edeb
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-07-27 01:16:27 +0530

    secon

--- myfirstdjangoapp/settings.py
+++ myfirstdjangoapp/settings.py
@@ -123,7 +123,7 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
                     os.path.join(PROJECT_ROOT, 'static'),
