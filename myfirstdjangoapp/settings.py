7428a4a91769687dc0e37ab3d6b8b3d3c2effb82
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-07-27 09:06:09 +0530

    s

--- myfirstdjangoapp/settings.py
+++ myfirstdjangoapp/settings.py
@@ -131,13 +131,13 @@ STATICFILES_DIRS = (
                     )
 
 
-BASE_DIR = dirname(dirname(abspath(__file__)))
+
 
 STATIC_ROOT = 'static' # Important for Heroku
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-                    path.join(BASE_DIR, 'static'),  # Important for Heroku
+                    os.path.join(BASE_DIR, 'static'),  # Important for Heroku
                     )
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
